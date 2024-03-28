50e5e5dc67e72f0dc267885ff04cd67f5097fad9
Your Name <alessanrospedroso@outlook.com>
2024-03-28 17:42:54 -0300

    Trabalhando com listas

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <cronometro :tempoEmSegundos="tempoEmSegundos" />
+        <Botao @clicando="iniciar" icone="fas fa-play" texto="play" :desabilitando="cronometroRodando" />
+        <Botao @clicando="finalizar" icone="fas fa-stop" texto="stop" :desabilitando="!cronometroRodando" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
+import Botao from './Botao.vue'
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Temporizador',
+    emits:['aoTemporizadorFinalizado'],
+    data(){
+        return {
+            tempoEmSegundos:0,
+            cronometro:0,
+            cronometroRodando: false
+        }
+    },
+    components:{Cronometro,Botao},
+    methods: {
+        iniciar() {
+            // começar a contagem
+            // 1 seg = 1000 ms
+            this.cronometroRodando = true;
+            this.cronometro = setInterval(()=>{
+                // console.log('incrementando o contador')
+                this.tempoEmSegundos +=1
+            },1000)
+
+            // console.log('iniciando');
+        },
+
+        finalizar() {
+            // console.log('finalizando');
+            this.cronometroRodando = false;
+            clearInterval(this.cronometro);
+            this.$emit('aoTemporizadorFinalizado',this.tempoEmSegundos);
+            this.tempoEmSegundos = 0
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
